db29f707ca5f1eeee8e6f8428d4ed0d6e7de5e39
ansaev <ansaev@gmail.com>
2016-10-14 00:28:52 +0300

    tests uncomment

--- tests.py
+++ tests.py
@@ -5,68 +5,68 @@ FILE_2_PATH = '/tmp/my_test_2.txt'
 FILE_CHECK_PATH = '/tmp/my_test_check.txt'
 
 
-# class TestArrayFunction:
-#     def test_simple_merge(self):
-#         assert sort_merge([1, 2, 5], [2, 3]) == [1, 2, 2, 3, 5]
-#
-#     def test_empty_input(self):
-#         assert sort_merge([1, 2, 5], []) == [1, 2, 5]
-#
-#     def test_int_input(self):
-#         try:
-#             sort_merge([1, 2, 5], 1)
-#         except BaseException as ex:
-#             assert isinstance(ex, TypeError)
-#             return
-#         assert False
-#
-#     def test_set_input(self):
-#         try:
-#             sort_merge({1, 2}, [1, 2, 5])
-#         except BaseException as ex:
-#             assert isinstance(ex, TypeError)
-#             return
-#         assert False
-#
-#
-# class TestGeneratorFunction:
-#     def test_simple_merge(self):
-#         make_test_files(FILE_1_PATH, FILE_2_PATH, FILE_CHECK_PATH, 1000)
-#         f1 = open(FILE_1_PATH, 'r')
-#         f2 = open(FILE_2_PATH, 'r')
-#         f_check = open(FILE_CHECK_PATH, 'r')
-#         f1_iter = iter((int(l) for l in f1))
-#         f2_iter = iter((int(l) for l in f2))
-#         f_check_iter = iter((int(l) for l in f_check))
-#         res = iter_sort_merge(f1_iter, f2_iter)
-#         for i in res:
-#             assert i == f_check_iter.__next__()
-#
-#     def test_empty_merge(self):
-#         make_test_files(FILE_1_PATH, FILE_2_PATH, FILE_CHECK_PATH, 1)
-#         f1 = open(FILE_1_PATH, 'r')
-#         f2 = open(FILE_2_PATH, 'r')
-#         f_check = open(FILE_CHECK_PATH, 'r')
-#         f1_iter = iter((int(l) for l in f1))
-#         f2_iter = iter((int(l) for l in f2))
-#         f_check_iter = iter((int(l) for l in f_check))
-#         res = iter_sort_merge(f1_iter, f2_iter)
-#         for i in res:
-#             assert i == f_check_iter.__next__()
-#
-#     def test_big_merge(self):
-#         ELEMENTS = 1000000
-#         make_test_files(FILE_1_PATH, FILE_2_PATH, FILE_CHECK_PATH, ELEMENTS)
-#         f1 = open(FILE_1_PATH, 'r')
-#         f2 = open(FILE_2_PATH, 'r')
-#         f_check = open(FILE_CHECK_PATH, 'r')
-#         f1_iter = iter((int(l) for l in f1))
-#         f2_iter = iter((int(l) for l in f2))
-#         f_check_iter = iter((int(l) for l in f_check))
-#         res = iter_sort_merge(f1_iter, f2_iter)
-#         for res_element in res:
-#             check_element = f_check_iter.__next__()
-#             assert res_element == check_element
+class TestArrayFunction:
+    def test_simple_merge(self):
+        assert sort_merge([1, 2, 5], [2, 3]) == [1, 2, 2, 3, 5]
+
+    def test_empty_input(self):
+        assert sort_merge([1, 2, 5], []) == [1, 2, 5]
+
+    def test_int_input(self):
+        try:
+            sort_merge([1, 2, 5], 1)
+        except BaseException as ex:
+            assert isinstance(ex, TypeError)
+            return
+        assert False
+
+    def test_set_input(self):
+        try:
+            sort_merge({1, 2}, [1, 2, 5])
+        except BaseException as ex:
+            assert isinstance(ex, TypeError)
+            return
+        assert False
+
+
+class TestGeneratorFunction:
+    def test_simple_merge(self):
+        make_test_files(FILE_1_PATH, FILE_2_PATH, FILE_CHECK_PATH, 1000)
+        f1 = open(FILE_1_PATH, 'r')
+        f2 = open(FILE_2_PATH, 'r')
+        f_check = open(FILE_CHECK_PATH, 'r')
+        f1_iter = iter((int(l) for l in f1))
+        f2_iter = iter((int(l) for l in f2))
+        f_check_iter = iter((int(l) for l in f_check))
+        res = iter_sort_merge(f1_iter, f2_iter)
+        for i in res:
+            assert i == f_check_iter.__next__()
+
+    def test_empty_merge(self):
+        make_test_files(FILE_1_PATH, FILE_2_PATH, FILE_CHECK_PATH, 1)
+        f1 = open(FILE_1_PATH, 'r')
+        f2 = open(FILE_2_PATH, 'r')
+        f_check = open(FILE_CHECK_PATH, 'r')
+        f1_iter = iter((int(l) for l in f1))
+        f2_iter = iter((int(l) for l in f2))
+        f_check_iter = iter((int(l) for l in f_check))
+        res = iter_sort_merge(f1_iter, f2_iter)
+        for i in res:
+            assert i == f_check_iter.__next__()
+
+    def test_big_merge(self):
+        ELEMENTS = 1000000
+        make_test_files(FILE_1_PATH, FILE_2_PATH, FILE_CHECK_PATH, ELEMENTS)
+        f1 = open(FILE_1_PATH, 'r')
+        f2 = open(FILE_2_PATH, 'r')
+        f_check = open(FILE_CHECK_PATH, 'r')
+        f1_iter = iter((int(l) for l in f1))
+        f2_iter = iter((int(l) for l in f2))
+        f_check_iter = iter((int(l) for l in f_check))
+        res = iter_sort_merge(f1_iter, f2_iter)
+        for res_element in res:
+            check_element = f_check_iter.__next__()
+            assert res_element == check_element
 
 
 class TestMergeSort:
